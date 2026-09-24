d143c6c25f8ab72c1e9ef04d331650e1c551af42
Kundan62055 <kk6205585@gmail.com>
2026-09-24 07:54:48 +0530

    Update CRUD application

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 import express from "express";
 import mongoose from "mongoose";
-import bodyParser from "body-parser"
-import dotenv from "dotenv"
-import cors from 'cors'
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from "cors";
 import route from "./routes/userRoute.js";
 
 const app = express();
@@ -10,7 +13,7 @@ app.use(bodyParser.json());
 app.use(cors());
 dotenv.config();
 
-const PORT = process.env.PORT || 7000
+const PORT = process.env.PORT || 5000
 const URL = process.env.MONGOURL;
 
 mongoose.connect(URL).then(()=>{
